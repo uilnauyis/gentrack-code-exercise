b0fbf55ee4a22f450d4e5b604c95026e03ae528e
uilnauyis <uilnauyis@gmail.com>
2020-07-26 17:44:40 +1200

    Updates

---
+++ precache-manifest.b971392feadcd7172a23851c8b28d5d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb57ef6965f27eb33557eba7d34e5307",
+    "url": "/gentrack-code-exercise/index.html"
+  },
+  {
+    "revision": "ce26443e755196f3dd34",
+    "url": "/gentrack-code-exercise/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "57d81deb72c8e656e28b",
+    "url": "/gentrack-code-exercise/static/css/main.711391f2.chunk.css"
+  },
+  {
+    "revision": "ce26443e755196f3dd34",
+    "url": "/gentrack-code-exercise/static/js/2.9aad086e.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/gentrack-code-exercise/static/js/2.9aad086e.js.LICENSE.txt"
+  },
+  {
+    "revision": "57d81deb72c8e656e28b",
+    "url": "/gentrack-code-exercise/static/js/main.c9acb6db.js"
+  },
+  {
+    "revision": "28da786db93d34db9cb1",
+    "url": "/gentrack-code-exercise/static/js/runtime-main.3efd24d0.js"
+  }
+]);
